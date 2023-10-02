a631004b0aae7ae0ef23e47d70d30b4c5a7bbc5d
Sidney Hays <me@sidneyhays.com>
2023-10-02 13:52:16 -0400

    Now clones and builds the ryzen_smu kernel module on install.

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# Clone and build ryzen_smu for Rembrandt
+# use the original ryzen_smu repo for any other ryzen cpu
+# https://github.com/leogx9r/ryzen_smu
+git clone https://gitlab.com/moson-mo/ryzen_smu.git
+make -C $DIR/ryzen_smu
+
 mkdir -p /usr/lib/asus-ryzen-power
 cp $DIR/lib/* /usr/lib/asus-ryzen-power/
+cp $DIR/ryzen_smu/ryzen_smu.ko /usr/lib/asus-ryzen-power/
 mkdir -p /usr/local/lib/systemd/system
 cp $DIR/service/* /usr/local/lib/systemd/system/
 systemctl enable --now asus-ryzen-power.timer
